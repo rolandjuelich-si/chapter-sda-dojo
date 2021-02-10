d4d65a50f318ecb0be03596aac53f8a4ec4f6cc3
si-sklaholz <sven.klaholz@signal-iduna.de>
2021-02-10 08:33:20 +0000

    Test #2

---
+++ src/test/java/de/signaliduna/chapter/sda/dojo/MyAwesomeTest.java
@@ -0,0 +1,12 @@
+package de.signaliduna.chapter.sda.dojo;
+
+import org.assertj.core.api.BDDAssertions;
+import org.junit.Test;
+
+public class MyAwesomeTest {
+    
+    @Test
+    public void shouldFail() {
+        BDDAssertions.fail("failureMessage");
+    }
+}
