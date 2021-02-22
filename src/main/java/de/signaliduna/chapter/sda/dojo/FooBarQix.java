ff04a753c6fe8feaec72de10f2257c20e5dd9f1b
Peter Gagelmann <peter.gagelmann@signal-iduna.de>
2021-02-22 08:23:32 +0100

    Build refactor

--- src/main/java/de/signaliduna/chapter/sda/dojo/FooBarQix.java
+++ src/main/java/de/signaliduna/chapter/sda/dojo/FooBarQix.java
@@ -3,9 +3,9 @@ package de.signaliduna.chapter.sda.dojo;
 public class FooBarQix {
 
   public String compute(String givenNumber) {
-    if ("1".equals(givenNumber) || "2".equals(givenNumber) || "4".equals(givenNumber)) {
-      return givenNumber;
-    }
-    return "Foo";
+    if ("6".equals(givenNumber) || "9".equals(givenNumber) || "12".equals(givenNumber)) {
+        return "Foo";
+      }
+    return givenNumber;
   }
 }
