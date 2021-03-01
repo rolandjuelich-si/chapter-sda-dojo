12c96cdaccb33fe2722d5dde094a358dc9694d71
Peter Gagelmann <peter.gagelmann@signal-iduna.de>
2021-03-01 08:25:22 +0100

    2. stage 1.3.21

--- src/test/java/de/signaliduna/chapter/sda/dojo/FooBarQixShould.java
+++ src/test/java/de/signaliduna/chapter/sda/dojo/FooBarQixShould.java
@@ -10,69 +10,43 @@ import org.junit.runner.RunWith;
 @RunWith(JUnitParamsRunner.class)
 public class FooBarQixShould {
 
-  @Test
-  public void returnNumberForOne() {
-    //given
-    final String givenNumber = "1";
+	@Test
+	@Parameters({ "1", "2", "4" })
+	public void returnNumberForDefault(final String givenNumber) {
+		// when
+		final String actual = new FooBarQix().compute(givenNumber);
 
-    //when
-    final String actual = new FooBarQix().compute(givenNumber);
+		// then
+		then(actual).isEqualTo(givenNumber);
+	}
 
-    //then
-    then(actual).isEqualTo("1");
-  }
+	@Test
+	@Parameters({ "6", "9", "12" })
+	public void returnFooForDivisibleByThree(final String givenNumber) {
+		// when
+		final String actual = new FooBarQix().compute(givenNumber);
 
-  @Test
-  public void returnNumberForTwo() {
-    //given
-    final String givenNumber = "2";
+		// then
+		then(actual).isEqualTo("Foo");
+	}
 
-    //when
-    final String actual = new FooBarQix().compute(givenNumber);
+	@Test
+	@Parameters({ "5", "10", "20" })
+	public void returnBarForDivisibleByFive(final String givenNumber) {
+		// when
+		final String actual = new FooBarQix().compute(givenNumber);
 
-    //then
-    then(actual).isEqualTo("2");
-  }
+		// then
+		then(actual).isEqualTo("Bar");
+	}
 
-  @Test
-  public void returnNumberForFour() {
-    //given
-    final String givenNumber = "4";
+	@Test
+	@Parameters({ "14", "28", "49" })
+	public void returnQixForDivisibleBySeven(final String givenNumber) {
+		// when
+		final String actual = new FooBarQix().compute(givenNumber);
 
-    //when
-    final String actual = new FooBarQix().compute(givenNumber);
-
-    //then
-    then(actual).isEqualTo("4");
-  }
-
- @Test
-  @Parameters({"6", "9", "12"})
-  public void returnFooForDivisibleByThree(final String givenNumber) {
-    //when
-    final String actual = new FooBarQix().compute(givenNumber);
-
-    //then
-    then(actual).isEqualTo("Foo");
-  }
-  
-  @Test
-  @Parameters({"5", "10", "20"})
-  public void returnBarForDivisibleByFive(final String givenNumber) {
-    //when
-    final String actual = new FooBarQix().compute(givenNumber);
-
-    //then
-    then(actual).isEqualTo("Bar");
-  }
-
-  @Test
-  @Parameters({"14", "28", "49"})
-  public void returnQixForDivisibleBySeven(final String givenNumber) {
-    //when
-    final String actual = new FooBarQix().compute(givenNumber);
-
-    //then
-    then(actual).isEqualTo("Qix");
-  }
+		// then
+		then(actual).isEqualTo("Qix");
+	}
 }
