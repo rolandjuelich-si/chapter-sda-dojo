a34aa671122a54dfb26145e60b6bd8ee3ccd38a5
Roland Jülich <roland.juelich@signal-iduna.de>
2021-03-15 08:35:27 +0100

    add unit test for foobarqix

---
+++ src/main/java/de/signaliduna/chapter/sda/dojo/FooBarQix.java
@@ -0,0 +1,21 @@
+package de.signaliduna.chapter.sda.dojo;
+
+public class FooBarQix {
+
+  public String compute(final String givenNumber) {
+    final int number = Integer.parseInt(givenNumber);
+    if (number % 3 == 0) {
+      return "Foo";
+    }
+
+    if (number % 5 == 0) {
+      return "Bar";
+    }
+
+    if (number % 7 == 0) {
+      return "Qix";
+    }
+
+    return givenNumber;
+  }
+}
---
+++ src/test/java/de/signaliduna/chapter/sda/dojo/FooBarQixShould.java
@@ -0,0 +1,19 @@
+package de.signaliduna.chapter.sda.dojo;
+
+import org.junit.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+
+public class FooBarQixShould {
+
+  @Test
+  public void testName() throws Exception {
+    // given
+    Divisibility divisibility = Mockito.mock(Divisibility.class);
+    // when
+    divisibility.compute("");
+    // then
+    BDDMockito.then(divisibility).should().compute(Mockito.anyString());
+  }
+
+}
