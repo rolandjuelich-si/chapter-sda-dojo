8510c28e6b8029745bf3a1c0c353e2f06a67974a
Stefan Sailer <stefan.sailer@signal-iduna.de>
2021-04-19 08:50:16 +0200

    interface

--- src/main/java/de/signaliduna/chapter/sda/dojo/FooBarQix.java
+++ src/main/java/de/signaliduna/chapter/sda/dojo/FooBarQix.java
@@ -1,13 +1,18 @@
 package de.signaliduna.chapter.sda.dojo;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class FooBarQix implements Computable {
 
   private final Divisibility divisibility;
   private final Occurrence occurrence;
+    private final List<Computable> computables;
 
-  public FooBarQix(final Divisibility divisibility, final Occurrence occurrence) {
+    public FooBarQix(final Divisibility divisibility, final Occurrence occurrence) {
     this.divisibility = divisibility;
     this.occurrence = occurrence;
+    this.computables = Arrays.asList(new Divisibility(), new Occurrence());
   }
 
   @Override
